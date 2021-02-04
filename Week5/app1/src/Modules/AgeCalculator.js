88bcedd92c01326934a15123aa31c22a0363e3b1
Gerald Aden <gerald@gmatech.com>
2021-02-03 16:53:58 -0800

    Switched to using moment for age calculation.

--- Week5/app1/src/Modules/AgeCalculator.js
+++ Week5/app1/src/Modules/AgeCalculator.js
@@ -1,12 +1,21 @@
+var moment = require('moment'); // node.js require
+
+// function getAge(day, month, year)
+// {
+//     var today = new Date();
+//     var birthDate = new Date(year, month, day);
+//     var age = today.getFullYear() - birthDate.getFullYear();
+//     var m = today.getMonth() - birthDate.getMonth();
+//     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+//         age--;
+//     }
+//     return age;
+// }
+
 function getAge(day, month, year)
 {
-    var today = new Date();
-    var birthDate = new Date(year, month, day);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
+    var age = moment().diff(`${year}-${month}-${day}`, 'years');
+
     return age;
 }
 
