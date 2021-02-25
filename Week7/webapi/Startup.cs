985f6ded15f4ebece632b284ccb7730960b55539
Gerald Aden <gerald@gmatech.com>
2021-02-24 16:55:30 -0800

    Removed CORS and  https redirect for development.

--- Week7/webapi/Startup.cs
+++ Week7/webapi/Startup.cs
@@ -43,9 +43,18 @@ namespace webapi
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "webapi v1"));
+            
+                app.UseCors(builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                );
             }
 
-            app.UseHttpsRedirection();
+            if (!env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
