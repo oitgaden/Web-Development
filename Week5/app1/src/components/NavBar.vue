c8b6cb585484c0bd2d0d67f7e6d89e3ba68442c6
oitgaden <gerald.aden@oit.edu>
2021-03-11 20:09:20 -0800

    Removed experimental code.

--- Week5/app1/src/components/NavBar.vue
+++ Week5/app1/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="links" v-if="loggedIn">
+    <div class="links">
         <span v-on:click='goToHome'>Home</span>
         <span v-on:click='goToTables'>Table Example</span>
         <span v-on:click='goToForms'>Form Example</span>
@@ -9,9 +9,6 @@
 <script>
   export default {
     name: 'NavBar',
-    props: {
-      loggedIn: Boolean
-    },
     methods: {
       goToHome: function() {
         this.$router.push({ path: 'home' })
